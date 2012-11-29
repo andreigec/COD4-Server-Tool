4aa66b1e40d687bf41226cbfd946559a5e8f555e
andrei <andreigec@hotmail.com>
2012-11-29 14:53:03 +1100

    initial 1.1

---
+++ SOURCE/Cod4ServerTool/controller.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ANDREICSLIB;
+using Protocol;
+
+namespace Cod4ServerTool
+{
+    public static class controller
+    {
+        private static void extractServers(Servers serversInstance, threads threadsInstance, byte[] data)
+        {
+            if (data == null)
+                return;
+            String datastr = Encoding.ASCII.GetString(data);
+
+            var seps = new string[1];
+            seps[0] = "\\";
+            string[] ips = datastr.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (String s in ips)
+            {
+                if (s.Length != 6)
+                    continue;
+
+                String ip = "";
+                ip += ((int) s[0]).ToString() + ".";
+                ip += ((int) s[1]).ToString() + ".";
+                ip += ((int) s[2]).ToString() + ".";
+                ip += ((int) s[3]).ToString();
+
+                int port = (s[4])*256 + (s[5]);
+
+                threadsInstance.addServerThread(ip, port, serversInstance);
+            }
+        }
+
+        public static void masterServerAdd(ToolStripLabel toolStripLabel, StatusStrip SS, Servers serversInstance,
+                                           threads threadsInstance)
+        {
+            //we need to increase the 15th byte to change the region
+            var gs = Protocol.Protocol.cod4getservers;
+            for (int region = 0; region < 6; region++)
+            {
+                toolStripText(toolStripLabel, ref SS, "Adding region:" + region + "/6");
+                byte[] rec = NetUpdates.SendUDPPacketGetBlockingResponse(NetUpdates.GetIPFromHostname(Protocol.Protocol.MasterServer), 20810, gs);
+                gs[15]++;
+
+                extractServers(serversInstance, threadsInstance, rec);
+            }
+
+            toolStripText(toolStripLabel, ref SS, "Ready");
+        }
+
+        public static void toolStripText(ToolStripLabel toolStripLabel, ref StatusStrip SS, String text)
+        {
+            toolStripLabel.Text = text;
+            SS.Invalidate();
+            SS.Update();
+        }
+
+
+        public static Image getEmbeddedImage(String name)
+        {
+            return Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Cod4ServerTool." + name));
+        }
+
+       public static int getLatencyDrop(ComboBox CB)
+        {
+            String s = CB.Text;
+            return int.Parse(s.Remove(0, 1));
+        }
+
+        public static void addServerToServerView(Server s, ListView serverView)
+        {
+            string port = s.getVariable(Protocol.Protocol.portSTR);
+            string ip = s.getVariable(Protocol.Protocol.ipAddressSTR);
+
+            var LVI = new ListViewItem();
+            String ipport = ip+Server.separator+port;
+            LVI.Name = ipport;
+            LVI.Text = s.getVariable(Protocol.Protocol.ipAddressSTR);
+
+            for (int a = 1; a < serverView.Columns.Count; a++)
+            {
+                if (serverView.Columns[a].Name == Protocol.Protocol.portSTR)
+                    LVI.SubItems.Add(port);
+                else
+                    LVI.SubItems.Add("");
+            }
+            serverView.Items.Add(LVI);
+            updateServerInServerView(s, serverView);
+        }
+
+        public static int getColumnIndex(ListView.ColumnHeaderCollection CHC, String name)
+        {
+            int a = 0;
+            foreach (ColumnHeader ch in CHC)
+            {
+                if (ch.Name == name)
+                    return a;
+                a++;
+            }
+            return -1;
+        }
+        
+        public static void updateServerInServerView(Server s, ListView serverView)
+        {
+            String ipport = s.getServerIdentifier(Protocol.Protocol.ipAddressSTR, Protocol.Protocol.portSTR);
+            int count = 0;
+            foreach (ColumnHeader CH in serverView.Columns)
+            {
+                String val = s.getVariable(CH.Name);
+                if (string.IsNullOrEmpty(val) == false)
+                    serverView.Items[ipport].SubItems[count].Text = val;
+                else
+                    serverView.Items[ipport].SubItems[count].Text = "";
+                count++;
+            }
+        }
+        
+        public static void updateFavourite(ListView serverView, Servers serversInstance, bool isFavourite)
+        {
+            foreach (ListViewItem LVI in serverView.SelectedItems)
+            {
+                Server s = serversInstance.masterServers[LVI.Name];
+                s.setVariable(Protocol.Protocol.favouriteSTR, isFavourite.ToString());
+                updateServerInServerView(s, serverView);
+            }
+        }
+        
+        //refresh selected items in listview
+        public static void refreshselected(ListView serverView, Protocol.Protocol protocolInstance, Servers serversInstance)
+        {
+            Server serv = serversInstance.masterServers[serverView.SelectedItems[0].Name];
+
+            protocolInstance.updateServerInfo(serv, Protocol.Protocol.basic);
+
+            updateServerInServerView(serv, serverView);
+        }
+        
+        public static void serversFromFile(ToolStripLabel toolStripLabel, StatusStrip SS, Servers serversInstance,
+                                           threads threadsInstance, ListView serverView, view baseView)
+        {
+            var OFD = new OpenFileDialog();
+            OFD.Title = "Select file to extract ip:ports from";
+            OFD.ShowDialog();
+            if (string.IsNullOrEmpty(OFD.FileName))
+                return;
+
+            var FS = new FileStream(OFD.FileName, FileMode.Open);
+            var SR = new StreamReader(FS);
+
+            String file = SR.ReadToEnd();
+            SR.Close();
+            FS.Close();
+
+            threadsInstance.addServerCollectionThread(toolStripLabel, ref SS, file, serversInstance, threadsInstance);
+
+            toolStripText(toolStripLabel, ref SS, "Ready");
+            baseView.allServersToServerView();
+        }
+
+        public static void serversFromURL(ToolStripLabel toolStripLabel, StatusStrip SS, Servers serversInstance,
+                                          threads threadsInstance, ListView serverView, view baseView)
+        {
+            var SB = new getStringBox();
+            SB.label1.Text = "Enter URL to extract ip:ports from";
+            SB.ShowDialog();
+            String ret = SB.returnvalue;
+            if (ret == "")
+                return;
+
+            toolStripText(toolStripLabel, ref SS, "Extracting Web Page Text");
+            String page = NetUpdates.DownloadWebPage(ret);
+
+            if (page == null)
+            {
+                MessageBox.Show("Not a valid webpage");
+                return;
+            }
+
+
+            threadsInstance.addServerCollectionThread(toolStripLabel, ref SS, page, serversInstance, threadsInstance);
+
+
+            toolStripText(toolStripLabel, ref SS, "Ready");
+            baseView.allServersToServerView();
+        }
+       
+
+        public static void changeDirectory(String exe)
+        {
+            String s = exe;
+            s = s.Substring(0, s.LastIndexOf("\\"));
+
+            Environment.CurrentDirectory = s;
+        }
+
+        public static void connecttoserver(String ipaddr, String gameLocation)
+        {
+            String codfile = gameLocation;
+            if (File.Exists(codfile) == false)
+            {
+                var OFD = new OpenFileDialog();
+                OFD.Multiselect = false;
+                OFD.Title = "Select iw3mp.exe to run an ip address with";
+                OFD.ShowDialog();
+                view.resetDirectory();
+
+                if (OFD.FileName == null || OFD.FileName.Length == 0)
+                    return;
+
+                var FS = new FileStream(codfile, FileMode.Create);
+                var SW = new StreamWriter(FS);
+
+                SW.WriteLine(OFD.FileName);
+
+                SW.Close();
+                FS.Close();
+            }
+
+            String codlocation = "";
+            try
+            {
+                var FS = new FileStream(codfile, FileMode.Open);
+                var SR = new StreamReader(FS);
+
+                codlocation = SR.ReadToEnd();
+                codlocation = codlocation.Substring(0, codlocation.IndexOf("iw3mp.exe") + 9);
+
+                SR.Close();
+                FS.Close();
+
+                changeDirectory(codlocation);
+
+                Process.Start(codlocation, "connect " + ipaddr);
+            }
+            catch
+            {
+                MessageBox.Show("An error has occurred starting:" + codlocation);
+                File.Delete(codfile);
+            }
+        }
+
+        public static void deleteSelected(ListView serverView, Servers serversInstance)
+        {
+            ListView.SelectedListViewItemCollection SLVIC = serverView.SelectedItems;
+
+            int count = 0;
+            String ips = "";
+            foreach (ListViewItem LVI in SLVIC)
+            {
+                if (count > 10)
+                {
+                    ips += "...";
+                    break;
+                }
+                count++;
+                ips += LVI.Name + "\n";
+            }
+
+
+            DialogResult DR = MessageBox.Show("Are you sure you want to delete these ips?\n" + ips, "question",
+                                              MessageBoxButtons.YesNo);
+            if (DR == DialogResult.No)
+                return;
+
+
+            foreach (ListViewItem LVI in SLVIC)
+            {
+                serverView.Items.Remove(LVI);
+                serversInstance.masterServers.Remove(LVI.Name);
+            }
+        }
+
+        public static bool isInt(String s)
+        {
+            var R = new Regex("^[0-9]+$");
+            return R.IsMatch(s);
+        }
+    }
+}
---
+++ SOURCE/Cod4ServerTool/extraInfo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ANDREICSLIB;
+using Protocol;
+
+namespace Cod4ServerTool
+{
+    public partial class extraInfo : Form
+    {
+        private readonly view baseView;
+
+        public Server thisServer;
+
+        public extraInfo(view newBaseView)
+        {
+            InitializeComponent();
+            baseView = newBaseView;
+        }
+
+        private void update(String[] split)
+        {
+            var ip = thisServer.getVariable(Protocol.Protocol.ipAddressSTR);
+            var port = thisServer.getVariable(Protocol.Protocol.portSTR);
+            var hostname = thisServer.getVariable(Protocol.Protocol.hostNameSTR);
+            var mapname = thisServer.getVariable(Protocol.Protocol.mapNameSTR);
+            var clients = thisServer.getVariable(Protocol.Protocol.clientsSTR);
+            var maxclients = thisServer.getVariable(Protocol.Protocol.maxClientsSTR);
+            var gametype = thisServer.getVariable(Protocol.Protocol.gameTypeSTR);
+            var pb = thisServer.getVariable(Protocol.Protocol.punkBusterSTR);
+            var maxping = thisServer.getVariable(Protocol.Protocol.maxPingSTR);
+            var ping = thisServer.getVariable(Protocol.Protocol.pingSTR);
+
+
+            extra.Items.Clear();
+            serveriptext.Text =ip+Server.separator+port;
+            hostnametext.Text = hostname;
+            mapnametext.Text = mapname;
+
+            clientstext.Text = string.IsNullOrEmpty(clients) ? "0" : clients;
+
+            maxclientstext.Text = maxclients;
+            gametypetext.Text = gametype;
+            maxpingtext.Text = maxping;
+
+            if (pb == "1")
+                punkbustercheck.Checked = true;
+            else
+                punkbustercheck.Checked = false;
+
+            latencytext.Text = ping;
+
+            if (split == null)
+                return;
+
+            for (int a = 0; a < split.Length; a += 2)
+            {
+                String s = split[a];
+                String s2 = split[a + 1];
+
+                //check if players
+                var r = new Regex(@"[0-9]+\n");
+                Match M = r.Match(s2);
+                if (M.Length != 0)
+                {
+                    string[] z = Regex.Split(s2, @"[0-9]+\n");
+                    s2 = s2.Substring(0, s2.IndexOf('\n'));
+                    var seps = new string[1];
+                    seps[0] = "\n";
+                    string[] players = z[1].Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (players.Length == 0)
+                        break;
+
+
+                    playerlistview.Items.Clear();
+                    foreach (String player in players)
+                    {
+                        var sepp = new string[1];
+                        sepp[0] = "\"";
+                        string[] sas = player.Split(sepp, StringSplitOptions.RemoveEmptyEntries);
+                        MatchCollection mc = Regex.Matches(player, @"([0-9]+).+?([0-9]+)");
+
+
+                        string arg1 = sas[0].Substring(0, sas[0].IndexOf(' '));
+                        string arg2 = sas[0].Substring(sas[0].IndexOf(' ') + 1);
+
+                        int arg1i = int.Parse(arg1);
+                        int arg2i = int.Parse(arg2);
+
+                        var LVI = new ListViewItem();
+
+                        LVI.Text = Protocol.Protocol.sanatiseName(sas[1]);
+                        LVI.SubItems.Add(arg1i.ToString());
+                        LVI.SubItems.Add(arg2i.ToString());
+                        playerlistview.Items.Add(LVI);
+                    }
+                }
+                else
+                {
+                    var LVI = new ListViewItem();
+                    LVI.Text = s;
+                    LVI.SubItems.Add(s2);
+                    extra.Items.Add(LVI);
+                }
+            }
+        }
+
+        private void updateServer()
+        {
+            string[] args = baseView.protocolInstance.updateServerInfo(thisServer, Protocol.Protocol.extended);
+
+            update(args);
+        }
+
+        private void extraInfo_Load(object sender, EventArgs e)
+        {
+            updateServer();
+        }
+
+        private void closebutton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void connectbutton_Click(object sender, EventArgs e)
+        {
+            controller.connecttoserver(thisServer.getServerIdentifier(Protocol.Protocol.ipAddressSTR, Protocol.Protocol.portSTR),
+                                       baseView.gamelocation);
+            view.resetDirectory();
+        }
+
+        private void refreshbutton_Click(object sender, EventArgs e)
+        {
+            updateServer();
+        }
+
+        private void copyToClipboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (extra.SelectedItems.Count != 1)
+                return;
+
+            Clipboard.SetText(extra.SelectedItems[0].SubItems[1].Text);
+        }
+
+    }
+}
